059f904d96fc6f355ab7f7e4e6540fe25c49ed57
mariusl93 <mariusl@getacademy.no>
2021-09-09 15:01:55 +0200

    Initial commit

---
+++ SimpleChess/Bishop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChess
+{
+    class Bishop : Piece
+    {
+        public Bishop() : base ("LPR") //konstruktoren arver fra Piece klassen(base) sin konstruktor, protected Piece(string symbol) Symbol = symbol; Setter Symbol Stringen til "LPR"
+        {
+        }
+
+
+
+        public override bool Move(string fromPosition, string toPosition) //we use the override keyword to override the base class method.
+                                                                          //Fordi Move er abstract og abstract metoder må være i abstract class.
+                                                                          //An override method provides a new implementation of the method inherited from a base class.
+                                                                          //The method that is overridden by an override declaration is known as the overridden base method.
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+            return Math.Abs(diffRow) == Math.Abs(diffCol);
+        }
+    }
+}
---
+++ SimpleChess/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleChess
+{
+    abstract class Piece //Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class).
+    {
+        public string Symbol { get; }
+
+        protected Piece(string symbol) // A protected member is accessible within its class and by derived class instances.
+        {
+            Symbol = symbol;
+        }
+
+        public abstract bool Move(string fromPosition, string toPosition); //Abstract method: can only be used in an abstract class, and it does not have a body.
+                                                                           //The body is provided by the derived class (inherited from).
+
+
+    }
+}
+
+
+
+
+
